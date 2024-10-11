faf10f61b8f66d1bcceffc6eb3f5c28b9634d11e
garikhg <garik.28h@gmail.com>
2024-10-12 03:16:46 +0400

    Update contact block content

--- wp-content/themes/wpnext/template-parts/blocks/contact-details/template.php
+++ wp-content/themes/wpnext/template-parts/blocks/contact-details/template.php
@@ -23,8 +23,102 @@ if ( ! empty( $block['className'] ) ) {
 if ( ! empty( $block['align'] ) ) {
 	$className .= ' align' . $block['align'];
 }
+
+// Global
+$title       = get_field( 'title' ) ?? '';
+$description = get_field( 'description' ) ?? '';
+
+// Addresses
+$addresses        = get_field( 'addresses' ) ?? '';
+$street_addresses = $addresses['addresses'] ?? '';
+
+// Contacts
+$contacts        = get_field( 'contacts' ) ?? '';
+$contact_details = $contacts['contact_details'] ?? '';
+
+
+// Working Hours
+$working_hours = get_field( 'working_hours' ) ?? '';
+$hours         = $working_hours['hours'] ?? '';
+
 ?>
 
-<section id="<?php echo esc_attr( $id ) ?>" class="<?php echo esc_attr( $className ) ?>">
-	Contact Block
-</section>
+<div class="<?php echo esc_attr( $className ) ?>">
+	<div class="block-contact__header">
+		<?php if ( ! empty( $title ) ): ?>
+			<h3 class="block-contact__heading">
+				<?php echo $title ?>
+			</h3>
+		<?php endif; ?>
+
+		<?php if ( ! empty( $description ) ): ?>
+			<div class="block-contact__description">
+				<?php echo wp_kses_post( $description ); ?>
+			</div>
+		<?php endif; ?>
+	</div>
+
+	<div class="block-contact__content">
+		<?php if ( 0 < count( $addresses ) ): ?>
+			<div class="block-contact__addresses">
+				<?php if ( ! empty( $addresses['title'] ) ): ?>
+					<h5 class="block-contact__item-title">
+						<?php echo $addresses['title']; ?>
+					</h5>
+				<?php endif; ?>
+
+				<?php if ( 0 < count( $street_addresses ) ): ?>
+					<div class="block-contact__item-list">
+						<?php foreach ( $street_addresses as $address ): ?>
+							<div class="block-contact__item-info">
+								<?php echo wp_kses_post( $address['street'] ); ?>
+							</div>
+						<?php endforeach; ?>
+					</div>
+				<?php endif; ?>
+			</div>
+		<?php endif; // endif Addresses ?>
+
+		<?php if ( 0 < count( $contacts ) ): ?>
+			<div class="block-contact__contacts">
+				<?php if ( ! empty( $contacts['title'] ) ): ?>
+					<h5 class="block-contact__item-title">
+						<?php echo $contacts['title']; ?>
+					</h5>
+				<?php endif; ?>
+
+				<?php if ( 0 < count( $contact_details ) ): ?>
+					<div class="block-contact__item-list">
+						<?php foreach ( $contact_details as $contact ): ?>
+							<div class="block-contact__item-info">
+								<?php echo $contact['label']; ?>
+								<?php echo $contact['contact']; ?>
+							</div>
+						<?php endforeach; ?>
+					</div>
+				<?php endif; ?>
+			</div>
+		<?php endif; // endif Contacts ?>
+
+		<?php if ( 0 < count( $working_hours ) ): ?>
+			<div class="block-contact__working-hours">
+				<?php if ( ! empty( $working_hours['title'] ) ): ?>
+					<h5 class="block-contact__item-title">
+						<?php echo $working_hours['title']; ?>
+					</h5>
+				<?php endif; ?>
+
+				<?php if ( 0 < count( $hours ) ): ?>
+					<div class="block-contact__item-list">
+						<?php foreach ( $hours as $hour ): ?>
+							<div class="block-contact__item-info">
+								<?php echo $hour['hours']; ?>
+							</div>
+						<?php endforeach; ?>
+					</div>
+				<?php endif; ?>
+			</div>
+		<?php endif; // endif Working Hours ?>
+	</div>
+
+</div>
