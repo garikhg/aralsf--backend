e9a416f5d4dbae2a5f1e64a60c44eacc37d5b333
garikhg <garik.28h@gmail.com>
2024-11-03 23:07:38 +0400

    Update template index file

--- wp-content/themes/aral-distribution/index.php
+++ wp-content/themes/aral-distribution/index.php
@@ -16,9 +16,31 @@
 get_header();
 ?>
 
-    <div class="container">
-        Index Page
+<?php if ( have_posts() ): ?>
+    <header class="page-header has-breadcrumbs">
+        <div class="container">
+			<?php aral_distribution_breadcrumbs() ?>
+        </div>
+    </header>
+
+    <div class="page-content container">
+		<?php the_archive_title( '<h1 class="archive-title page-title">', '</h1>' ); ?>
+        <div class="text-gray-600 flex justify-between mb-8">
+			<?php aral_distribution_the_products_showing() ?>
+        </div>
+        <div class="products grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+			<?php while ( have_posts() ) {
+				the_post();
+				get_template_part( 'template-parts/content/content-product' );
+			} ?>
+        </div>
     </div>
+
+<?php else: ?>
+	
+	<?php get_template_part( 'template-parts/content/content-none' ); ?>
+
+<?php endif; ?>
 
 <?php
 get_footer();
