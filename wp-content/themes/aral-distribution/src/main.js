59c7e841094ce9615cf53b21692db995a9e3179f
garikhg <garik.28h@gmail.com>
2024-11-03 22:45:02 +0400

    Added mobile submenu show/hidden function

--- wp-content/themes/aral-distribution/src/main.js
+++ wp-content/themes/aral-distribution/src/main.js
@@ -9,5 +9,20 @@ import {
 initTWE( { Offcanvas, Ripple, Dropdown } );
 
 document.addEventListener( "DOMContentLoaded", function () {
-    // console.log("loaded");
+    const mobileMenu = document.getElementById( "primary-mobile-menu" );
+    if ( mobileMenu ) {
+        const menuItems = mobileMenu.querySelectorAll( ".menu-item-has-children" );
+        menuItems.forEach( function ( menuItem ) {
+            const menuItemButton = menuItem.querySelector( "li.menu-item-has-children > a" );
+            const menuItemSubmenu = menuItem.querySelector( ".sub-menu" );
+
+            menuItemButton.addEventListener( "click", function ( event ) {
+                event.preventDefault();
+                if ( menuItemSubmenu ) {
+                    event.currentTarget.closest( "li.menu-item-has-children" ).classList.toggle( "show" );
+                    menuItemSubmenu.classList.toggle( "show" );
+                }
+            } );
+        } );
+    }
 } );
