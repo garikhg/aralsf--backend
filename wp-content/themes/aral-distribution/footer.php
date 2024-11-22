36c61136b8a9546c6b0901e78d304166b22ff23c
garikhg <garik.28h@gmail.com>
2024-11-23 03:06:49 +0400

    Fixed footer grid and columns

--- wp-content/themes/aral-distribution/footer.php
+++ wp-content/themes/aral-distribution/footer.php
@@ -46,24 +46,22 @@ $copyright = ! empty( $copyright ) ? str_replace( '{Y}', date_i18n( 'Y' ), $copy
 
 <footer class="w-full bg-primary text-primary-foreground mt-auto">
     <div class="container py-8 lg:py-16 space-y-8 lg:space-y-16">
-
-        <div class="flex flex-wrap space-y-8 lg:space-y-0">
-            <div class="hidden lg:block footer-column w-full lg:w-1/4">
-                <div class="space-y-4 lg:space-y-8">
-					<?php get_template_part( 'template-parts/header/brand-logo' ) ?>
-					
-					<?php if ( ! empty( $footer_description ) ): ?>
-                        <div class="footer-description font-light xl:pr-16">
-							<?php echo wp_kses_post( $footer_description ); ?>
-                        </div>
-					<?php endif; ?>
-                </div>
+        <div class="grid grid-cols-1 lg:grid-cols-4 gap-8 lg:gap-16">
+           
+            <div class="col-span-1 lg:col-span-1 space-y-4 lg:space-y-6">
+				<?php get_template_part( 'template-parts/header/brand-logo' ); ?>
+				
+				<?php if ( ! empty( $footer_description ) ): ?>
+                    <div class="footer-description font-light">
+						<?php echo wp_kses_post( $footer_description ); ?>
+                    </div>
+				<?php endif; ?>
             </div>
 
-            <div class="footer-column w-full lg:w-3/4">
-
-                <div class="flex flex-wrap justify-end space-y-8 md:space-y-0 lg:space-x-24 -mx-4">
-                    <div class="footer-block w-full md:w-1/3 lg:w-auto px-4">
+            <div class="col-span-1 lg:col-span-3 flex flex-col lg:flex-row justify-end gap-8 lg:gap-16">
+                <div class="flex flex-col gap-8 lg:flex-row lg:gap-16">
+                    <div class="separator h-px w-full lg:h-full lg:w-px lg:hidden bg-white/30"></div>
+                    <div class="relative">
 						<?php if ( $footer_menu_info ): ?>
                             <h5 class="footer-block__title">
 								<?php echo wp_get_nav_menu_name( 'footer' ); ?>
@@ -71,15 +69,23 @@ $copyright = ! empty( $copyright ) ? str_replace( '{Y}', date_i18n( 'Y' ), $copy
 							<?php echo $footer_menu_info; ?>
 						<?php endif; ?>
                     </div>
-                    <div class="footer-block w-full md:w-1/3 lg:w-auto px-4">
-						<?php if ( $footer_menu_cat ): ?>
+                </div>
+
+                <div class="flex flex-col gap-8 lg:flex-row lg:gap-16">
+                    <div class="separator h-px w-full lg:h-full lg:w-px bg-white/30"></div>
+                    <div class="relative">
+						<?php if ( $footer_menu_info ): ?>
                             <h5 class="footer-block__title">
 								<?php echo wp_get_nav_menu_name( 'footer_categories' ); ?>
                             </h5>
 							<?php echo $footer_menu_cat; ?>
 						<?php endif; ?>
                     </div>
-                    <div class="footer-block w-full md:w-1/3 lg:w-auto px-4 space-y-8">
+                </div>
+
+                <div class="flex flex-col gap-8 lg:flex-row lg:gap-16">
+                    <div class="separator h-px w-full lg:h-full lg:w-px bg-white/30"></div>
+                    <div class="relative space-y-8">
 						<?php if ( $contact_blocks ): ?>
 							<?php foreach ( $contact_blocks as $block ): ?>
                                 <div class="footer-block__contact">
@@ -97,10 +103,10 @@ $copyright = ! empty( $copyright ) ? str_replace( '{Y}', date_i18n( 'Y' ), $copy
             </div>
         </div>
 
-        <div class="grid grid-cols-1 md:grid-cols-2 items-center border-t border-white/30 pt-8">
+        <div class="grid grid-cols-1 lg:grid-cols-2 items-center border-t border-white/30 pt-8">
             <div class="col-span-1">
 				<?php if ( ! empty( $copyright ) ): ?>
-                    <div class="font-light text-center md:text-start">
+                    <div class="copyright font-light text-center md:text-start">
 						<?php echo wp_kses_post( $copyright ); ?>
                     </div>
 				<?php endif; ?>
@@ -108,6 +114,7 @@ $copyright = ! empty( $copyright ) ? str_replace( '{Y}', date_i18n( 'Y' ), $copy
 
             <div class="col-span-1"></div>
         </div>
+
     </div>
 </footer>
 </div><!-- #page -->
